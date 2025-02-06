87acb92a81f488dd02c15bbe5134f8a8d0e3527c
Zane <zmatarieh@bsd220.org>
2025-02-06 09:43:52 -0600

    added pizza icon and link store to website

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -6,19 +6,79 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    @State private var startPosition = MapCameraPosition.userLocation(
+        fallback: .automatic
+    )
+    @State private var places = [Place]()
+    @State private var mapRegion = MKCoordinateRegion()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Map(position: $startPosition) {
+
+                   UserAnnotation()
+
+                   ForEach(places) { place in
+
+                       Annotation(place.mapItem.name!, coordinate: place.mapItem.placemark.coordinate) {
+
+                                           if let url = place.mapItem.url {
+
+                                               Link(destination: url, label: {
+
+                                                   Image("pizza")
+
+                                               })
+
+                                           }
+
+                                       }
+
+                   }
+
+               }
+        .onMapCameraChange { context in
+
+                    mapRegion = context.region
+
+                    performSearch(item: "Pizza")
+
+                }
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = mapRegion
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if let response = response {
+                places.removeAll()
+                
+                for mapItem in response.mapItems {
+                    
+                    places.append(Place(mapItem: mapItem))
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
+    
+    
+    struct Place: Identifiable {
+
+        let id = UUID()
+
+        let mapItem: MKMapItem
+
+    }
