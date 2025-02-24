3aa2c6aa5a0c9d2afc3ec6be990c958bdd700a5d
Zane <zmatarieh@bsd220.org>
2025-02-23 23:42:38 -0600

    added map sesarhc feature

---
+++ LocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailsView.swift
+//  'Za Hunter
+//
+//  Created by Zane Matarieh on 2/13/25.
+//
+
+import SwiftUI
+
+struct LocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailsView()
+}
